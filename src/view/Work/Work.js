c52ba69cb25e226f9ca23172afbf11a3ffd85255
bellsarian <bellasarian@gmail.com>
2022-07-05 11:59:11 -0700

    Open pop up click work item

--- src/view/Work/Work.js
+++ src/view/Work/Work.js
@@ -9,6 +9,14 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
+// Material UI
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+
 WebFont.load({
   google: {
     families: ["Ramabhadra", "sans-serif", "Red Hat Text"],
@@ -16,7 +24,47 @@ WebFont.load({
 });
 
 export default class Work extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedWork: new Object(),
+      workDialogState: false,
+      paragraph: [],
+    };
+  }
+  componentDidMount = () => {
+    this.setState({ selectedWork: WorkData[0] });
+  };
+
+  handleClickWork = (workObject) => {
+    this.setState({ selectedWork: workObject });
+    this.handleWorkDialog();
+  };
+
+  handleWorkDialog = () => {
+    this.setState({ workDialogState: !this.state.workDialogState });
+  };
+
+  workParagraph = () => {
+    let work = this.state.selectedWork.jobDescription;
+    if (work != undefined && work.length > 0) {
+      return work.map((item, index) => (
+        <div key={"work-item-" + index}>{item}</div>
+      ));
+    }
+  };
+
+  workSkills = () => {
+    let work = this.state.selectedWork.skills;
+    if (work != undefined && work.length > 0) {
+      return work.map((item, index) => (
+        <span key={"work-item-" + index}>{item} </span>
+      ));
+    }
+  };
+
   render() {
+    let { selectedWork, workDialogState, paragraph } = this.state;
     return (
       <div className="WorkContainer">
         <div className="WorkTextContainer">
@@ -30,30 +78,35 @@ export default class Work extends Component {
                 content = (
                   <div
                     className="timeline-element-container"
-                    onClick={(event) => console.log("ITEM CLICKED " + Date())}
+                    onClick={(event) => this.handleClickWork(item)}
                   >
                     <div className="timeline-element-content">
                       <div className="company ">{item.companyName}</div>
                       <div className="position">{item.position}</div>
                       <div className="role">{item.roles}</div>
-                      <div className="skills">{item.skills}</div>
+                      <div className="role">{item.date}</div>
+                      {/* <div className="skills">{item.skills}</div> */}
                     </div>
-                    <div className="timeline-element-arrow">
+                    <div className="timeline-element-arrow-left">
                       <img src="timelineArrow.png" alt="arrow" />
                     </div>
                   </div>
                 );
               } else {
                 content = (
-                  <div className="timeline-element-container">
-                    <div className="timeline-element-arrow">
+                  <div
+                    className="timeline-element-container"
+                    onClick={(event) => this.handleClickWork(item)}
+                  >
+                    <div className="timeline-element-arrow-right">
                       <img src="timelineArrowLeft.png" alt="arrow" />
                     </div>
                     <div className="timeline-element-content">
                       <div className="company ">{item.companyName}</div>
                       <div className="position">{item.position}</div>
                       <div className="role">{item.roles}</div>
-                      <div className="skills">{item.skills}</div>
+                      <div className="role">{item.date}</div>
+                      {/* <div className="skills">{item.skills}</div> */}
                     </div>
                   </div>
                 );
@@ -61,6 +114,7 @@ export default class Work extends Component {
 
               return (
                 <VerticalTimelineElement
+                  key={"workitem-" + item.id}
                   className={
                     "timeline-element vertical-timeline-element--" + item.id
                   }
@@ -95,6 +149,38 @@ export default class Work extends Component {
               }}
             />
           </VerticalTimeline>
+
+          <Dialog
+            open={workDialogState}
+            keepMounted
+            onClose={this.handleWorkDialog}
+            aria-describedby="alert-dialog-slide-description"
+            PaperProps={{
+              style: {
+                backgroundColor: "#131313",
+                color: "#fff",
+                border: "#343434",
+                minWidth: "30vw",
+              },
+            }}
+          >
+            <DialogTitle>{selectedWork.companyName}</DialogTitle>
+            <DialogContent>
+              <div className="work-dialog-container">
+                <div>{selectedWork.position}</div>
+                <div>{selectedWork.roles}</div>
+                {/* <span>Skills: </span> */}
+                {this.workSkills()}
+                <div>{selectedWork.date}</div>
+                <div>{selectedWork.location}</div>
+
+                {this.workParagraph()}
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleWorkDialog}>Close</Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     );
